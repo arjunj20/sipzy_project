92d464ce0d9b8c72dd005b02176013716b66c645
Arjun <your-email@example.com>
2026-01-07 10:57:16 +0530

    feature: implemented the chart and top selling products, brands, categories

--- dashboard/views.py
+++ dashboard/views.py
@@ -6,14 +6,33 @@ from django.db.models import Sum
 from django.views.decorators.cache import never_cache
 from django.utils import timezone
 from datetime import datetime, timedelta
+from authenticate.models import CustomUser
+from django.db.models import Count
+from orders.models import Order
 
 
 @never_cache
 def admin_dashboard(request):
     if not request.user.is_authenticated or not request.user.is_superuser:
         return redirect('admin_login')
+    users_count = CustomUser.objects.aggregate(count=Count("fullname"))
+
+    total_orders = Order.objects.aggregate(order_count=Count("uuid"))
+    top_selling_products = OrderItem.objects.exclude(status__in=['cancelled', 'returned']).values('product__name').annotate(total_sold=Sum('quantity')).order_by('-total_sold')[:10]
+    top_selling_categories = OrderItem.objects.exclude(status__in=["cancelled", "returned"]).values("product__category__name").annotate(total_sold=Sum("quantity")).order_by("-total_sold")[:11]
+    top_selling_brands = OrderItem.objects.exclude(status__in=["cancelled", "returned"]).values("product__brand__name").annotate(total_sold=Sum("quantity")).order_by("-total_sold")[:10]
     
-    return render(request, "dashboard/admin_dashboard.html")
+        
+    context = {
+        "total_users": users_count['count'],
+        "total_orders": total_orders["order_count"],
+        "top_selling_products": top_selling_products,
+        "top_selling_categories": top_selling_categories,
+        "top_selling_brands": top_selling_brands,
+
+    }
+    
+    return render(request, "dashboard/admin_dashboard.html", context)
 
 
 def visualization_data(request):
@@ -22,7 +41,6 @@ def visualization_data(request):
 
     grouping_method = request.GET.get("grouping_method", "Daily")
 
-    # Get paid order items excluding cancelled/returned
     query_set = OrderItem.objects.filter(
         order__payment_status="paid"
     ).exclude(
